91dca604a9d10192c7dec017fa880ad76e893d6e
sidmadethis <sidmadethis@gmail.com>
2017-06-05 10:11:21 -0600

    changed settings page

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """A class to store all settings for ALien Invasion."""
+
+    def __init__(self):
+        """initialize a game's settings"""
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
+
+#we are doing this to import this file into the main alien/invasions file. this way when we change stuff it is easier to find.
