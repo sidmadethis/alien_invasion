509b6360c4f39faa5d7d4c156aabd59b4e56ae0d
sidmadethis <sidmadethis@gmail.com>
2017-06-06 20:52:16 -0600

    added the ship image and wrote a ship class

---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Ship():
+    """initialize the ship and set its starting position"""
+    self.screen = screen
+
+    #load the ship image and get its rect.
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+
+    #start each new ship at the bottom center of the screen
+    self.rect.centerx = self.screen_rect.centerx
+    self.rect.bottom = self.screen_rect.bottom
+
+def blitme(self):
+    """draw the ship at its current location"""
+    self.screen.blit(self.image, self.rect)
+    
